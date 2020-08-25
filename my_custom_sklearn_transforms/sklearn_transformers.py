80c57a5544b37ffeeedf5642d476d319ab792060
Tania Incio <tincio.gomez@gmail.com>
2020-08-25 02:14:31 -0500

    add imputer values

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -54,3 +54,30 @@ class StandardData():
         # normalizamos datos, con desviacion estandar = 1
         data = StandardScaler().fit_transform(data)
         return data
+        
+
+class ImputerValues():
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        # replace null values according to the analysis of plots for each feature
+        data['EFECTIVO'].replace(np.nan, data['EFECTIVO'].median(), inplace = True)
+        data['CXC'].replace(np.nan, data['CXC'].median(), inplace = True)
+        data['INVENTARIO'].replace(np.nan, data['INVENTARIO'].median(), inplace = True)
+        data['EQ_OFICINA'].replace(np.nan, data['EQ_OFICINA'].median(), inplace = True)
+        data['EQ_TRANSPORTE'].replace(np.nan, data['EQ_TRANSPORTE'].median(), inplace = True)
+        data['ANTICIPOS_CTE'].replace(np.nan, data['ANTICIPOS_CTE'].median(), inplace = True)
+        data['CXP'].replace(np.nan, data['CXP'].median(), inplace = True)
+        data['CAP_SOCIAL'].replace(np.nan, data['CAP_SOCIAL'].median(), inplace = True)
+        data['TERRENOS_Y_CONSTRUCCIONES'].replace(np.nan, data['TERRENOS_Y_CONSTRUCCIONES'].median(), inplace = True)
+        data['CONTRIBUCIONES_X_PAGAR'].replace(np.nan, data['CONTRIBUCIONES_X_PAGAR'].median(), inplace = True)
+        data['TOTAL_COMPRAS'].replace(np.nan, data['TOTAL_COMPRAS'].median(), inplace = True)
+        data['TOTAL_GASTOS'].replace(np.nan, data['TOTAL_GASTOS'].median(), inplace = True)
+        data['TOTAL_VENTAS'].replace(np.nan, data['TOTAL_VENTAS'].median(), inplace = True)
+        data['UTILIDAD_BRUTA'].replace(np.nan, data['UTILIDAD_BRUTA'].median(), inplace = True)
+
+        return data
